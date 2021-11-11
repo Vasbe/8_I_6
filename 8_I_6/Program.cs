25a30de364a4730db7b6f279ad16cf035fcff643
Vasbe <vasbeme@gmail.com>
2021-11-11 12:18:11 +0400

    Добавьте файлы проекта.

---
+++ 8_I_6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _8_I_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторку s: ");
+            string s = Console.ReadLine();
+            int index = 0;
+            for (int i = 0;i < s.Length; i++) {
+                if (s.Substring(i, 1) != " ") {
+                    index++;
+                }
+            }
+            Console.WriteLine("Всего букв в строке: " + index);
+        }
+    }
+}
